466828dd2c220ec93db16b28c4fa18eb00cc28c8
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-07-07 15:30:47 +0200

    solved console methods challenge

--- js-basics-and-variables/js-basics-and-variables_console-methods/index.js
+++ js-basics-and-variables/js-basics-and-variables_console-methods/index.js
@@ -19,3 +19,17 @@ console.clear();
 // 7. Which method is used to display an error message in the console? Use it do display the error message in the console.
 
 // Well done!
+
+const coding = "Happy coding!";
+
+console.log(coding);
+
+const hacking = "Happy Hacking!";
+
+console.log(hacking);
+
+console.clear();
+
+const error = "This is an error!";
+
+console.error(error);
