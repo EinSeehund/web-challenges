f19303e3182bcaad32e32801272868469d8b9f6c
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-07-10 16:52:07 +0200

    solve js scroll bar challenge

--- js-functions/js-functions_progress-bar/js/index.js
+++ js-functions/js-functions_progress-bar/js/index.js
@@ -2,4 +2,15 @@ console.clear();
 
 const progressBar = document.querySelector('[data-js="progress-bar"]');
 
-function calculateScrollPercentage() {}
+function calculateScrollPercentage() {
+    console.log(window.scrollY);
+    console.log(window.innerHeight);
+    console.log(document.body.clientHeight);
+    const progressInPercent =
+        (100 / (document.body.clientHeight - window.innerHeight)) *
+        window.scrollY;
+
+    progressBar.style.width = `${progressInPercent}%`;
+}
+
+document.addEventListener("scroll", calculateScrollPercentage);
