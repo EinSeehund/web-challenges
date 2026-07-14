13d0b994b2c0e663fc48fa0d66b219f26b2ab60d
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-07-14 11:17:54 +0200

    solve js forms checkbox challenge

--- js-forms/js-forms_checkbox-input/js/index.js
+++ js-forms/js-forms_checkbox-input/js/index.js
@@ -3,22 +3,42 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const successMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
-  tosError.setAttribute("hidden", "");
+    tosError.setAttribute("hidden", "");
 }
 
 function showTosError() {
-  tosError.removeAttribute("hidden");
+    tosError.removeAttribute("hidden");
 }
 
+function showSuccess() {
+    successMessage.removeAttribute("hidden");
+}
+
+hideTosError();
+successMessage.setAttribute("hidden", "");
+
+tosCheckbox.addEventListener("input", () => {
+    if (tosCheckbox.checked) {
+        hideTosError();
+    } else {
+        showTosError();
+    }
+});
+
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  // --v-- write your code here --v--
+    // --v-- write your code here --v--
+    if (!tosCheckbox.checked) {
+        showTosError();
+        return;
+    }
+    // --^-- write your code here --^--
 
-  // --^-- write your code here --^--
-
-  // eslint-disable-next-line no-alert
-  alert("Form submitted");
+    // eslint-disable-next-line no-alert
+    showSuccess();
+    alert("Form submitted");
 });
