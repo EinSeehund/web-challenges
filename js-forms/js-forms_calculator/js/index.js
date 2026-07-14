ddcac3353a19064fb24a47a917fb9dd5788e538d
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-07-14 11:38:25 +0200

    solve js forms calculator challenge

--- js-forms/js-forms_calculator/js/index.js
+++ js-forms/js-forms_calculator/js/index.js
@@ -4,29 +4,56 @@ const form = document.querySelector('[data-js="form"]');
 const resultOutput = document.querySelector('[data-js="result"]');
 
 function add(a, b) {
-  return a + b;
+    return a + b;
 }
 
 function subtract(a, b) {
-  return a - b;
+    return a - b;
 }
 
 function multiply(a, b) {
-  return a * b;
+    return a * b;
 }
 
 function divide(a, b) {
-  return a / b;
+    return a / b;
 }
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 
-  let result;
+    let result;
 
-  // --v-- write your code here --v--
+    // --v-- write your code here --v--
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    const number1 = Number(data.numberA);
+    const number2 = Number(data.numberB);
 
-  // --^-- write your code here --^--
+    if (number1 === 0) {
+        event.target.elements.numberA.value = 0;
+    }
+    if (number2 === 0) {
+        event.target.elements.numberB.value = 0;
+    }
 
-  resultOutput.textContent = result;
+    switch (data.operator) {
+        case "addition":
+            result = add(number1, number2);
+            break;
+        case "subtraction":
+            result = subtract(number1, number2);
+            break;
+        case "multiplication":
+            result = multiply(number1, number2);
+            break;
+        case "division":
+            result = divide(number1, number2);
+            break;
+        default:
+            alert("Can not perform operation");
+    }
+    // --^-- write your code here --^--
+
+    resultOutput.textContent = result;
 });
