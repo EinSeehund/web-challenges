36f4d3ec5e48364b82aa40c3d30ad35396a6e596
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-07-20 21:48:39 +0200

    solve js array methods flag challenge

--- js-array-methods/js-array-methods_fun-with-flags/index.js
+++ js-array-methods/js-array-methods_fun-with-flags/index.js
@@ -5,14 +5,18 @@ const container = document.querySelector('[data-js="card-container"]');
 const queryInput = document.querySelector('[data-js="query-input"]');
 
 queryInput.addEventListener("input", (event) => {
-  container.innerHTML = "";
+    container.innerHTML = "";
 
-  const searchString = event.target.value;
+    const searchString = event.target.value;
 
-  const foundCountry = null;
+    const foundCountries = countries.filter((country) =>
+        country.name.toLowerCase().startsWith(searchString),
+    );
 
-  if (foundCountry) {
-    const countryElement = Country(foundCountry);
-    container.append(countryElement);
-  }
+    if (foundCountries) {
+        foundCountries.forEach((country) => {
+            const countryElement = Country(country);
+            container.append(countryElement);
+        });
+    }
 });
