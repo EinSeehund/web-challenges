cbe097a0b12a8cf7c198d9b9d7d9c9584074fabd
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-07-24 11:48:26 +0200

    solve js modern syntax bonus challenge back to basics

--- js-modern-syntax/js-modern-syntax_back-to-basics/js/index.js
+++ js-modern-syntax/js-modern-syntax_back-to-basics/js/index.js
@@ -3,15 +3,25 @@ Now that you've practiced destructuring, default parameters, and the spread oper
 try reversing the challenge by rewriting this code without using these modern features.
 */
 
-export const getNameAndCountry = ({ name, country }) => [name, country];
+//export const getNameAndCountry = ({ name, country }) => [name, country];
+export const getNameAndCountry = (cityObject) => [
+    cityObject.name,
+    cityObject.country,
+];
+
+// export const getRelocatedCity = (city1, city2 = { name: "Berlin", country: "Germany" },) => {
+
+//   const [, country] = getNameAndCountry(city2);
+//     return {...city1, country,};
+// };
 
 export const getRelocatedCity = (
-  city1,
-  city2 = { name: "Berlin", country: "Germany" }
+    city1,
+    city2 = { name: "Berlin", country: "Germany" },
 ) => {
-  const [, country] = getNameAndCountry(city2);
-  return {
-    ...city1,
-    country,
-  };
+    const city2Array = getNameAndCountry(city2);
+    const country = city2Array[1];
+    //const [, country] = getNameAndCountry(city2);
+    const city1Name = city1.name;
+    return { name: city1Name, country: country };
 };
