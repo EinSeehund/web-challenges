8646dc8e8a818517586f2c589d4f37d5c348aa3b
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-07-28 15:18:15 +0200

    solve react basics attributes challenge

--- react-basics/react-basics_attributes/src/App.jsx
+++ react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,22 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+    return <MyArticle />;
+}
+
+function MyArticle() {
+    return (
+        <article className="article">
+            <h2 className="article__title">Hello World!</h2>
+            <label htmlFor="my-input">Enter your name:</label>
+            <input id="my-input"></input>
+            <a
+                href="https://isitchristmas.today/"
+                rel="noreferrer"
+                target="_blank"
+            >
+                Is it Christmas today?
+            </a>
+        </article>
+    );
 }
