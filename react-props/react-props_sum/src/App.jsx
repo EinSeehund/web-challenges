edf39bb6b20615967a06a4d9e087f1989d5ccebf
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-07-30 10:56:52 +0200

    solve react props sum challenge

--- react-props/react-props_sum/src/App.jsx
+++ react-props/react-props_sum/src/App.jsx
@@ -1,3 +1,19 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+    return (
+        <div>
+            <Sum valueA={2} valueB={3} />
+            <Sum valueA={4} valueB={65} />
+            <Sum valueA={45} valueB={78} />
+            <Sum valueA={4321} valueB={6456} />
+            <Sum valueA={456456} valueB={456456} />
+        </div>
+    );
+}
+
+function Sum({ valueA, valueB }) {
+    return (
+        <h1>
+            {valueA} + {valueB} = {valueA + valueB}
+        </h1>
+    );
 }
