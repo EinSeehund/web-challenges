971c0a2377f2411d21985ad38b7e22e09ca5fff2
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-07-30 15:38:17 +0200

    solve react nesting navigation challenge

--- react-nesting/react-nesting_navigation/src/App.jsx
+++ react-nesting/react-nesting_navigation/src/App.jsx
@@ -1,35 +1,25 @@
 import "./styles.css";
-
 import avatar from "./img/avatar.jpg";
 import logo from "./img/logo.jpg";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Logo from "./components/Logo";
+import Avatar from "./components/Avatar";
+import Link from "./components/Link";
 
 export default function App() {
-  return (
-    <>
-      <header className="header">
-        <a href="#">
-          <img className="round-image" src={logo} alt="logo" />
-        </a>
-        <nav>
-          <a className="navigation__link" href="#home">
-            Home
-          </a>
-          <a className="navigation__link" href="#about">
-            About
-          </a>
-          <a className="navigation__link" href="#impressum">
-            Impressum
-          </a>
-        </nav>
-        <button
-          type="button"
-          onClick={() => console.log("I could toggle a profile!")}
-          aria-label="toggle profile"
-        >
-          <img className="round-image" src={avatar} alt="avatar" />
-        </button>
-      </header>
-      <main>content goes here…</main>
-    </>
-  );
+    return (
+        <>
+            <Header>
+                <Logo src={logo} />
+                <Navigation>
+                    <Link href="#home">Home</Link>
+                    <Link href="#about">About</Link>
+                    <Link href="#impressum">Impressum</Link>
+                </Navigation>
+                <Avatar src={avatar} />
+            </Header>
+            <main>content goes here…</main>
+        </>
+    );
 }
