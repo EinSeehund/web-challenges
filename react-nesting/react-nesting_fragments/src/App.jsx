36c4e63919b9e1ed5af521978d8f4f6a695adfd7
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-07-30 15:05:30 +0200

    solve react nesting fragments challenge

--- react-nesting/react-nesting_fragments/src/App.jsx
+++ react-nesting/react-nesting_fragments/src/App.jsx
@@ -3,11 +3,19 @@ import "./styles.css";
 import Box from "./components/Box";
 
 export default function App() {
-  return (
-    <main className="flex-container">
-      <Box color="#007bff" />
-      <Box color="#fc3" />
-      <Box color="#ff3333" />
-    </main>
-  );
+    return (
+        <main className="flex-container">
+            <Boxes />
+        </main>
+    );
+}
+
+function Boxes() {
+    return (
+        <>
+            <Box color="#007bff" />
+            <Box color="#fc3" />
+            <Box color="#ff3333" />
+        </>
+    );
 }
