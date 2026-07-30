e16926d16237589b761dfff32ea87588af300cf5
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-07-30 10:51:20 +0200

    solve react props greeting challenge

--- react-props/react-props_greeting/src/App.jsx
+++ react-props/react-props_greeting/src/App.jsx
@@ -1,3 +1,12 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+    return (
+        <div>
+            <Greeting name="my Friend" />
+            <Greeting name="Roland" />
+        </div>
+    );
+}
+
+function Greeting({ name }) {
+    return <h1>Hello, {name === "Roland" ? "Coach" : name}!</h1>;
 }
