1735743cf61e2e7a063ab8e93dbfd3c332eb82db
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-07-31 17:02:58 +0200

    solve react arrays animal filter challenge

--- react-arrays/react-with-arrays-filter-animals/src/App.jsx
+++ react-arrays/react-with-arrays-filter-animals/src/App.jsx
@@ -1,34 +1,62 @@
 import React from "react";
+import { useState } from "react";
 import "./styles.css";
 
 const animals = [
-  { id: "1", name: "Dog", habitat: "Domestic Animal", emoji: "🐕" },
-  { id: "2", name: "Cat", habitat: "Domestic Animal", emoji: "🐈" },
-  { id: "3", name: "Hamster", habitat: "Domestic Animal", emoji: "🐹" },
-  { id: "4", name: "Parrot", habitat: "Domestic Animal", emoji: "🦜" },
-  { id: "5", name: "Fish", habitat: "Domestic Animal", emoji: "🐠" },
+    { id: "1", name: "Dog", habitat: "Domestic Animal", emoji: "🐕" },
+    { id: "2", name: "Cat", habitat: "Domestic Animal", emoji: "🐈" },
+    { id: "3", name: "Hamster", habitat: "Domestic Animal", emoji: "🐹" },
+    { id: "4", name: "Parrot", habitat: "Domestic Animal", emoji: "🦜" },
+    { id: "5", name: "Fish", habitat: "Domestic Animal", emoji: "🐠" },
 
-  { id: "6", name: "Fox", habitat: "Forest", emoji: "🦊" },
-  { id: "7", name: "Bear", habitat: "Forest", emoji: "🐻" },
-  { id: "8", name: "Owl", habitat: "Forest", emoji: "🦉" },
-  { id: "9", name: "Deer", habitat: "Forest", emoji: "🦌" },
-  { id: "10", name: "Hedgehog", habitat: "Forest", emoji: "🦔" },
+    { id: "6", name: "Fox", habitat: "Forest", emoji: "🦊" },
+    { id: "7", name: "Bear", habitat: "Forest", emoji: "🐻" },
+    { id: "8", name: "Owl", habitat: "Forest", emoji: "🦉" },
+    { id: "9", name: "Deer", habitat: "Forest", emoji: "🦌" },
+    { id: "10", name: "Hedgehog", habitat: "Forest", emoji: "🦔" },
 
-  { id: "11", name: "Whale", habitat: "Ocean", emoji: "🐋" },
-  { id: "12", name: "Dolphin", habitat: "Ocean", emoji: "🐬" },
-  { id: "13", name: "Crab", habitat: "Ocean", emoji: "🦀" },
-  { id: "14", name: "Lobster", habitat: "Ocean", emoji: "🦞" },
-  { id: "15", name: "Shark", habitat: "Ocean", emoji: "🦈" },
+    { id: "11", name: "Whale", habitat: "Ocean", emoji: "🐋" },
+    { id: "12", name: "Dolphin", habitat: "Ocean", emoji: "🐬" },
+    { id: "13", name: "Crab", habitat: "Ocean", emoji: "🦀" },
+    { id: "14", name: "Lobster", habitat: "Ocean", emoji: "🦞" },
+    { id: "15", name: "Shark", habitat: "Ocean", emoji: "🦈" },
 
-  { id: "16", name: "Eagle", habitat: "Mountains", emoji: "🦅" },
-  { id: "17", name: "Goat", habitat: "Mountains", emoji: "🐐" },
-  { id: "18", name: "Wolf", habitat: "Mountains", emoji: "🐺" },
-  { id: "19", name: "Snow Leopard", habitat: "Mountains", emoji: "🐆" },
-  { id: "20", name: "Yak", habitat: "Mountains", emoji: "🐃" },
+    { id: "16", name: "Eagle", habitat: "Mountains", emoji: "🦅" },
+    { id: "17", name: "Goat", habitat: "Mountains", emoji: "🐐" },
+    { id: "18", name: "Wolf", habitat: "Mountains", emoji: "🐺" },
+    { id: "19", name: "Snow Leopard", habitat: "Mountains", emoji: "🐆" },
+    { id: "20", name: "Yak", habitat: "Mountains", emoji: "🐃" },
 ];
 
 const habitats = ["Mountains", "Ocean", "Forest", "Domestic Animal"];
 
 export default function App() {
-  return <h1>Animal Filter</h1>;
+    const [habitat, setHabitat] = useState("");
+    const animalsDisplay = animals.filter(
+        (animal) => animal.habitat === habitat,
+    );
+    return (
+        <>
+            <h1>Animal Filter</h1>
+
+            {habitats.map((habitatEl) => (
+                <button
+                    key={habitatEl}
+                    className="button"
+                    onClick={() => setHabitat(habitatEl)}
+                >
+                    {habitatEl}
+                </button>
+            ))}
+            <p>Selected habitat: {habitat}</p>
+            <ul>
+                {animalsDisplay.map((animalEl) => (
+                    <li key={animalEl.id}>
+                        {animalEl.name}
+                        {animalEl.emoji}
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
