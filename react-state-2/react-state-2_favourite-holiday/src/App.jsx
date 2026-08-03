73597e5223b4a1385d36bf5821675304edb5c3ee
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-08-03 11:27:53 +0200

    solve react state 2 holiday challenge

--- react-state-2/react-state-2_favourite-holiday/src/App.jsx
+++ react-state-2/react-state-2_favourite-holiday/src/App.jsx
@@ -1,39 +1,52 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+    const [holiday, setHoliday] = useState("Enter a holiday in the form above");
+    const [date, setDate] = useState("Enter a date in the form above");
 
-  return (
-    <div className="container">
-      <h1>Favourite Holiday Data Storage</h1>
-      <h2 id="favourite-holiday">Please tell us your favourite holiday!</h2>
-      <form
-        className="form"
-        aria-labelledby="favourite-holiday"
-        onSubmit={handleSubmit}
-      >
-        <label htmlFor="holiday">My Favourite Holiday: </label>
-        <input
-          id="holiday"
-          type="text"
-          name="holiday"
-          placeholder="e.g. Christmas"
-        />
-        <label htmlFor="date">Date: </label>
-        <input id="date" type="date" name="date" />
-        <button type="submit" className="form__submit-button">
-          Submit
-        </button>
-      </form>
-      <h2>Output of Submitted Data</h2>
-      <p>
-        Favourite Holiday: <span className="output">New Year</span>
-      </p>
-      <p>
-        Date: <span className="output">Well...</span>
-      </p>
-    </div>
-  );
+    function handleSubmit(event) {
+        event.preventDefault();
+        setHoliday(event.target.holiday.value);
+        setDate(event.target.date.value);
+
+        event.target.holiday.value = "";
+        event.target.date.value = "";
+
+        event.target.holiday.focus();
+    }
+
+    return (
+        <div className="container">
+            <h1>Favourite Holiday Data Storage</h1>
+            <h2 id="favourite-holiday">
+                Please tell us your favourite holiday!
+            </h2>
+            <form
+                className="form"
+                aria-labelledby="favourite-holiday"
+                onSubmit={handleSubmit}
+            >
+                <label htmlFor="holiday">My Favourite Holiday: </label>
+                <input
+                    id="holiday"
+                    type="text"
+                    name="holiday"
+                    placeholder="e.g. Christmas"
+                />
+                <label htmlFor="date">Date: </label>
+                <input id="date" type="date" name="date" />
+                <button type="submit" className="form__submit-button">
+                    Submit
+                </button>
+            </form>
+            <h2>Output of Submitted Data</h2>
+            <p>
+                Favourite Holiday: <span className="output">{holiday}</span>
+            </p>
+            <p>
+                Date: <span className="output">{date}.</span>
+            </p>
+        </div>
+    );
 }
