48ba5fe5df9a30c54a375885d8592fb921b99e15
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-08-06 11:01:28 +0200

    solve react custom hooks usemouse challenge

--- react-custom-hooks/react-custom-hooks-usemouse/src/hooks/useMouse.js
+++ react-custom-hooks/react-custom-hooks-usemouse/src/hooks/useMouse.js
@@ -1,1 +1,20 @@
-export default function useMouse() {}
+import { useEffect, useState } from "react";
+
+export default function useMouse() {
+    const [x, setX] = useState(500);
+    const [y, setY] = useState(500);
+
+    function updateCursor(event) {
+        setX(event.clientX);
+        setY(event.clientY);
+    }
+
+    useEffect(() => {
+        document.body.addEventListener("mousemove", updateCursor);
+
+        return () =>
+            document.body.removeEventListener("mousemove", updateCursor);
+    }, []);
+
+    return [x, y];
+}
