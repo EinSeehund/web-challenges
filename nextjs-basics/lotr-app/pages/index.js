6bb6510de59b48eecc33d6062f04340a58391d46
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-08-13 16:17:33 +0200

    solve next js basics challenge

--- nextjs-basics/lotr-app/pages/index.js
+++ nextjs-basics/lotr-app/pages/index.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <h1>Best Books Ever</h1>
+      <Link href="/volumes">Lord of the Rings</Link>
     </div>
   );
 }
