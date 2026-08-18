8e879125ce4e2a3bc51690682e46547818db9a70
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-08-18 12:36:05 +0200

    solve react global state challenge

--- react-global-state/react-global-state_lights/pages/rooms.js
+++ react-global-state/react-global-state_lights/pages/rooms.js
@@ -2,11 +2,11 @@ import Link from "../components/Link";
 import Lights from "../components/Lights";
 
 export default function Rooms({ lights, toggleLight }) {
-  return (
-    <>
-      <Link href="/">← Back home</Link>
-      <h1>All Rooms</h1>
-      <Lights lights={lights} toggleLight={toggleLight} />
-    </>
-  );
+    return (
+        <>
+            <Link href="/">← Back home</Link>
+            <h1>All Rooms</h1>
+            <Lights lights={lights} toggleLight={toggleLight} />
+        </>
+    );
 }
