576ff469f399f02b6654c6cbadee9f74e5ac61a8
Oleg Tjulenev <oleg.tjulenev@gmail.com>
2026-08-31 22:53:22 +0200

    solve backend create challenge

--- backend-create/backend-create_products/pages/api/products/index.js
+++ backend-create/backend-create_products/pages/api/products/index.js
@@ -2,14 +2,21 @@ import dbConnect from "@/db/connect";
 import Product from "@/db/models/Product";
 
 export default async function handler(request, response) {
-  await dbConnect();
+    await dbConnect();
 
-  if (request.method === "GET") {
-    const products = await Product.find();
+    if (request.method === "GET") {
+        const products = await Product.find();
 
-    response.status(200).json(products);
-    return;
-  }
+        response.status(200).json(products);
+        return;
+    }
 
-  response.status(405).json({ status: "Method not allowed." });
+    if (request.method === "POST") {
+        const productData = request.body;
+        await Product.create(productData);
+        response.status(201).json({ status: "Product created." });
+        return;
+    }
+
+    response.status(405).json({ status: "Method not allowed." });
 }
